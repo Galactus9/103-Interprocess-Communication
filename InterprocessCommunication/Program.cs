04f76ac7f2ecc32d7a6faac044fb47bca3bae3e7
Neoklis Varsamos <neoklis.varsamos@peoplecert.org>
2023-08-02 14:23:18 +0300

    Add project files.

---
+++ InterprocessCommunication/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+using System.Text;
+
+namespace InterprocessCommunication
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var one = Task.Run(async () =>
+            {
+                var server = new NamedPipeServerStream("testPipe", PipeDirection.InOut);
+
+                string clientExePath = "NamedPipeClientService.exe";
+
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = clientExePath,
+                };
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = startInfo;
+
+                    process.Start();
+
+                    Console.WriteLine("Server -- NamedPipeServerStream object created.\n");
+
+                    Console.WriteLine("Server -- Waiting for client connection...\n");
+
+                    server.WaitForConnection();
+
+                    Console.WriteLine("Server -- Client connected.\n");
+
+                    byte[] data = new byte[4];
+
+                    await server.ReadAsync(data, 0, 4);
+
+                    var lenght = BitConverter.ToInt32(data, 0);
+
+                    Console.WriteLine($"Server -- Received the size of the byte array from the Client.({lenght})\n");
+
+                    byte[] realData = new byte[lenght];
+
+                    await server.ReadAsync(realData, 0, lenght);
+
+                    Console.WriteLine("Server -- Receive the actual message in a byte array from the Client.\n");
+
+                    var str = Encoding.UTF8.GetString(realData);
+
+                    Console.WriteLine($"Server -- The message from the Client is : {str}.\n");
+
+                    await server.WriteAsync(new byte[] { 0 }, 0, 1);
+
+                    process.WaitForExit();
+                }
+            });
+            Task.WaitAll(one);
+        }
+    }
+}
---
+++ NamedPipeServerService/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO.Pipes;
+using System.Text;
+using System.Xml;
+
+namespace NamedPipeServerService
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var client = new NamedPipeClientStream(".", "testPipe", PipeDirection.InOut, PipeOptions.Asynchronous);
+
+            Console.WriteLine("Client -- Attempting to connect to pipe...\n");
+
+            await client.ConnectAsync();
+
+            Console.WriteLine("Client -- Connected to pipe.\n");
+
+            string someText = "Some text from the client";
+
+            var data = Encoding.UTF8.GetBytes(someText);
+
+            var lenghtData = BitConverter.GetBytes(data.Length);
+
+            Console.WriteLine("Client -- Send the size of the byte array to the server.\n");
+
+            await client.WriteAsync(lenghtData, 0, 4);
+
+            Console.WriteLine("Client -- Send the actual of the byte array to the server.\n");
+
+            await client.WriteAsync(data, 0, data.Length);
+
+            var a = new byte[1];
+
+            await client.ReadAsync(a, 0, 1);
+        }
+    }
+}
